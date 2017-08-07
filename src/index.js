f90e6f01ef9a4557ea983a4bf7be49148944846c
Dan Morrison <morrison.danr@gmail.com>
2017-08-06 21:47:48 -0400

    set up initial react application using Arnav Aggarwal's tutorial seen here https://codeburst.io/react-ecosystem-setup-step-by-step-walkthrough-721ff45a7fc1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+const container = document.getElementById('container');
+ReactDOM.render(<App></App>, container);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'app.bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'env']
+                    }
+                }
+            }
+        ]
+    }
+};
