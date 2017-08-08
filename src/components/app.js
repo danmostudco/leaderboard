55f6434b79ea380da568d703f48b6e465ebbbb1d
Dan Morrison <morrison.danr@gmail.com>
2017-08-07 21:55:18 -0400

    refactor app components to include export class App extends React.Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/app.css';
 
-export default function app() {
-    return (
+export default class App extends React.Component {
+    render() {
+        return (
         <div>
             <h1>Header</h1>
             <span>Lower span</span>
         </div>
-    );
+        );
+    }
 }
