e99cbec110b56742306942b6d5cb64710c08d040
Dan Morrison <morrison.danr@gmail.com>
2017-08-07 22:18:53 -0400

    set up componentDidMount() function to call loadData(), which fake sets state until I get an API.

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import '../styles/app.css';
 
+const people = ["Quinn", "Dan", "Bill"]
+
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {people: []};  
+    }
+
+    componentDidMount () {
+        this.loadData();
+    }
+
+    loadData() {
+        setTimeout(() => {
+            this.setState({ people: people });
+            // key for people is key in state
+            // value is referencing the people variable above
+        }, 500);
+    }
+    
     render() {
         return (
         <div>
