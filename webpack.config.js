ef83ca90168245338d8914c867d3ade3b3b34770
Dan Morrison <morrison.danr@gmail.com>
2017-08-14 21:56:52 -0400

    implement webpack-dev-server, allowing for an app bundle and a vendor bundle. Files can now be served to localhost:8000/app.html by typing in npm run watch
    this is done by adding webpack-dev-server to package.json, specifying
    within webpack.config.js to have an app and a vendor entry point that
    is bundled using the CommonsChunkPlugin (use a lone object),
    configuring the devServer within webpack.config.js, and then running
    the dev server from npm with the watch command.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+const webpack = require('webpack');
+
 const path = require('path');
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: {
+        app: path.resolve(__dirname, './src/index.js'),
+        vendor: ['react', 'react-dom']
+        },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'app.bundle.js',
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js'})
+    ],
     devtool: 'sourcemap',
+    devServer: {
+        port: 8000,
+        contentBase: 'dist' // where static files other than the bundles reside, will need to access 8000/app.html
+    },
     module: {
         rules: [
             {
